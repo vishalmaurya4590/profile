063c98bfb74b7d13cc8b181f24a804ea5a61cdb5
Vishal  Maurya <vishalmaurya886021@gmail.com>
2026-04-18 14:13:51 +0530

    Update project links to include branch information
    I mistakenly added older branch on it

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.querySelector("#projects .first-Project img").addEventListener("click", function(){
-    window.location.href = "https://github.com/vishalmaurya4590/ZeroKataGame";
+    window.location.href = "https://github.com/vishalmaurya4590/ZeroKataGame/tree/new-feature";
 })
 document.querySelector("#projects .second-Project img").addEventListener("click", function(){
     window.location.href = "https://github.com/vishalmaurya4590/Guss-the-number-game";
